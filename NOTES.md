39961c3d0560c661fe25b825e7d9dfee30fc0be1
Steven Jacobs <himself@stevenjacobs.be>
2021-05-12 23:16:40 +0200

    Module 2

---
+++ NOTES.md
@@ -0,0 +1,9 @@
+# notes
+
+## 2. The Site Class
+
+directory = self.dest / path.relative_to(self.source)
+
+    * The Path class has a __truediv__ method that returns another Path.
+    * You can do the same with your own classes.
+
--- ssg/site.py
+++ ssg/site.py
@@ -6,8 +6,8 @@ class Site:
         self.source = Path(source)
         self.dest = Path(dest)
 
-    def create_dir(self, path: Path):
-        directory = Path(self.dest, '/', path.relative_to(self.source))
+    def create_dir(self, path):
+        directory = self.dest / path.relative_to(self.source)
         directory.mkdir(parents=True, exist_ok=True)
 
     def build(self):
