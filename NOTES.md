2cb757083edd0b86a4abcc8475df0260b305dc20
Steven Jacobs <himself@stevenjacobs.be>
2021-05-13 09:51:27 +0200

    Module 3

--- NOTES.md
+++ NOTES.md
@@ -12,3 +12,19 @@ directory = self.dest / path.relative_to(self.source)
 extensions: List[str] = []
 
     * This is called an `annotation` of an empty List.
+
+## Module 3. The Content Class
+
+Lots of interesting things going on here:
+
+classmethod
+
+    * load() is a classmethod. Classmethods are used as factories that return a class object for different use cases. 
+        It is similar to other languages where functions with the same name are overloaded, and where the compiler knows which function to used based on the arguments alone. E.g. when a string, integer or object is passed.
+
+        A classmethod is also used with inheritance to ensure that the object that is created is an object of a child class.
+        A staticmethod does similar things than a classmethod. 
+        However, a staticmethod from a parent class always returns an object of that base class. 
+        A classmethod creates an instance of the child class if it was called on that child class.
+
+        See: https://www.programiz.com/python-programming/methods/built-in/classmethod
---
+++ ssg/content.py
@@ -0,0 +1,49 @@
+import re
+from yaml import load, FullLoader
+from collections.abc import Mapping
+
+
+class Content(Mapping):
+    __delimiter = r'^(?:-|\+){3}\s*$'
+    __regex = re.compile(__delimiter, re.MULTILINE)
+
+    @classmethod
+    def load(cls, string: str):
+        _, fm, content = cls.__regex.split(string, 2)
+        load(fm, Loader=FullLoader)
+        return cls(metadata, content)
+
+    def __init__(self, metadata: dict, content):
+        self.data = metadata
+        self.data.update({'content': content})
+
+    @property
+    def body(self):
+        return self.data['content']
+
+    @property
+    def type(self):
+        return self.data['type'] if 'type' in self.data else None
+
+    @type.setter
+    def type(self, value):
+        self.data['type'] = value
+
+    def __getitem__(self, key):
+        return self.data['key']
+
+    def __iter__(self):
+        return self.data.__getitem__
+
+    def __len__(self):
+        return len(self.data)
+
+    def __repr__(self):
+        data: dict = {}
+
+        #data = {key:value for key in data if key != 'content'}
+        for key, value in data:
+            if key != 'content':
+                data[key] = value
+
+        return str(data)
